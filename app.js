c7694a00c014f0f65b9d7932811ef421c05c6aa9
Ashwani Dewangan <105036643+ashwani312@users.noreply.github.com>
2023-05-11 14:02:12 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+function run(){
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+    let output = document.getElementById("output");
+
+
+    output.contentDocument.body.innerHTML = htmlCode + '<style>'+cssCode+'</style>';
+  
+}
+function runJs(){
+    let jsCode = document.getElementById("js-code").value;
+    let output = document.getElementById("output");
+    output.contentWindow.eval(jsCode);
+
+}
+
